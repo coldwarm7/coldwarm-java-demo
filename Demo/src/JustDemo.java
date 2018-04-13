a358c253f839638866abb7b8226d0714d79ff17c
coldwarm <1259404192@qq.com>
2018-04-13 20:05:57 +0800

    Demo
    M

--- Demo/src/JustDemo.java
+++ Demo/src/JustDemo.java
@@ -1,5 +1,7 @@
 public class JustDemo {
     public static void main(String[] args) {
         System.out.println("Win 10 git Demo");
+        System.out.println("Win 10 git Demo");
+        System.out.println("Win 10 git Demo");
     }
 }
