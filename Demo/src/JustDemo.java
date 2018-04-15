8f06512ade7ea5b13d0ec04a85c309c3b26fa5e6
coldwarm7 <1259404192@qq.com>
2018-04-15 12:13:57 +0800

    Delete JustDemo.java

--- Demo/src/JustDemo.java
+++
@@ -1,9 +0,0 @@
-public class JustDemo {
-    public static void main(String[] args) {
-        System.out.println("Win 10 git Demo");
-        System.out.println("Win 10 git Demo");
-        System.out.println("Win 10 git Demo");
-        System.out.println("Win 10 git Demo");
-        System.out.println("Win 10 git Demo");
-    }
-}
